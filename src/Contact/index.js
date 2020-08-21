48a939583a91ed4a92b565cb907a57307d34fe3d
diana <diliang4783573@gmail.com>
2020-08-21 15:21:49 -0700

    fixed responsive css to work for smaller screens (iphone 5/SE)

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -13,13 +13,15 @@ function Contact() {
     <div className="Contact">
       <h1 className="Contact-title">Let's keep in touch!</h1>
       <div className="Contact-container">
-        <a href={github}><FontAwesomeIcon icon={faGithub} size="3x"></FontAwesomeIcon></a>
-        <a href={linkedin}><FontAwesomeIcon icon={faLinkedin} size="3x"></FontAwesomeIcon></a>
-        <a href ="mailto: diliang4783573@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="3x"></FontAwesomeIcon></a>
+        <a href={github}><FontAwesomeIcon icon={faGithub} size="2x"></FontAwesomeIcon></a>
+        <a href={linkedin}><FontAwesomeIcon icon={faLinkedin} size="2x"></FontAwesomeIcon></a>
+        <a href ="mailto: diliang4783573@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="2x"></FontAwesomeIcon></a>
       </div>
-      {/* <div className="Contact-tele"><FontAwesomeIcon className="Contact-teleicon" icon={faPhone} /> 415-816-3646</div> */}
     </div>
   );
 };
+
+
+
 
 export default Contact;
