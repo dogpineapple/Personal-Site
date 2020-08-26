97cf3fe74d7bbee63ed8be0299ca97e6ebcd6e70
diana <diliang4783573@gmail.com>
2020-08-26 16:37:42 -0700

    updated image url

--- src/Hero/index.js
+++ src/Hero/index.js
@@ -3,6 +3,8 @@ import "./Hero.css";
 
 
 function Hero() {
+  const OWL = "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/17426035_1363415940410325_1423941097164729388_n.jpg?_nc_cat=104&_nc_sid=7aed08&_nc_ohc=7-GuvLROnqMAX9yRV_K&_nc_ht=scontent-sjc3-1.xx&oh=6f34e4421a515a93464d0fade6afe1aa&oe=5F66C862";
+  const PRO = "https://i.imgur.com/mRF0W1w.jpg";
   return (
     <div className="Hero">
       <div className="Hero-intro">
@@ -14,7 +16,7 @@ function Hero() {
         </p>
       </div>
       <div className="Hero-image-container">
-        <img className="Hero-image" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/17426035_1363415940410325_1423941097164729388_n.jpg?_nc_cat=104&_nc_sid=7aed08&_nc_ohc=7-GuvLROnqMAX9yRV_K&_nc_ht=scontent-sjc3-1.xx&oh=6f34e4421a515a93464d0fade6afe1aa&oe=5F66C862" alt="waterFfxiv" />
+        <img className="Hero-image" src={PRO} alt="portrait" />
       </div>
     </div>
   )
