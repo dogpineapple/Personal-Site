6fffa93b2a2fbbb61d3afc43ef1b37362f351274
diana <diliang4783573@gmail.com>
2020-09-24 16:08:35 -0700

    fixed about image and changed website styling

--- src/AboutHeader/index.js
+++ src/AboutHeader/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./AboutHeader.css";
+import * as monhunImg from "../images/monhun.JPG";
 
 function AboutHeader() {
 
   return (
     <div className="AboutHeader">
       <div className="AboutHeader-profilepic-cont">
-        <img className="AboutHeader-profilepic" src="https://scontent-sjc3-1.xx.fbcdn.net/v/t1.15752-9/p1080x2048/118310445_3225084867545741_7664178207222972142_n.jpg?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=jcXIQKHUWDUAX9OTXaT&_nc_ht=scontent-sjc3-1.xx&_nc_tp=6&oh=b14a413a916a8ac2fd80dbaa5bf411ad&oe=5F648340" alt="kaori.png"/>
+        <img className="AboutHeader-profilepic" src={monhunImg} alt="monhun.png"/>
         <p className="AboutHeader-profilepic-subtitle">*nervous laughs*</p>
       </div> 
       <div className="AboutHeader-background">
-        <h1>About -</h1>
+        <h1>About <span role="img" aria-label="sunflower">🌻</span> </h1>
         <p>My name is Dingren Liang, but please call me Diana!</p>
         <p>I am a recent graduate of <span className="AboutHeader-rithm">Rithm School's full-stack web development program</span>, based in San Francisco (Aug 2020).</p>
         <p>Before I started my journey with web development, I graduated from UC Davis with a B.S. in Global Disease Biology. At UC Davis, I was a research intern at a grape pathology lab in UC Davis, where I was introduced to R for data analysis.
--- src/Hero/index.js
+++ src/Hero/index.js
@@ -3,7 +3,6 @@ import "./Hero.css";
 
 
 function Hero() {
-  const OWL = "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/17426035_1363415940410325_1423941097164729388_n.jpg?_nc_cat=104&_nc_sid=7aed08&_nc_ohc=7-GuvLROnqMAX9yRV_K&_nc_ht=scontent-sjc3-1.xx&oh=6f34e4421a515a93464d0fade6afe1aa&oe=5F66C862";
   const PRO = "https://i.imgur.com/mRF0W1w.jpg";
   return (
     <div className="Hero">
