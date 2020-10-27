4f8e5171c5f498aac114a169d98ff59b55ad03f3
diana <diliang4783573@gmail.com>
2020-10-27 15:00:37 -0700

    changed hero image url to file in app

--- src/Hero/index.js
+++ src/Hero/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Hero.css";
+import * as dianaPortrait from "../images/dianaportrait.jpg";
 
 
 function Hero() {
-  const PRO = "https://i.imgur.com/mRF0W1w.jpg";
   return (
     <div className="Hero">
       <div className="Hero-intro">
@@ -15,7 +15,7 @@ function Hero() {
         </p>
       </div>
       <div className="Hero-image-container">
-        <img className="Hero-image" src={PRO} alt="portrait" />
+        <img className="Hero-image" src={dianaPortrait} alt="portrait" />
         <div className="Hero-image-square"/>
       </div>
     </div>
