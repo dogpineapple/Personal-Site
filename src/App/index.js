cfa9db2b71d54d9ee4bcf93cf26613483f6855d6
diana <diliang4783573@gmail.com>
2020-10-27 15:58:27 -0700

    added scrolling cat

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Projects from '../Projects';
 import Contact from '../Contact';
@@ -6,8 +6,25 @@ import Landing from '../Landing';
 import About from '../About';
 import TopNaviBar from '../TopNaviBar';
 import './App.css';
+import * as noscrollCat from "../images/noscrollcat.png";
+import * as scrollCat from "../images/scrollcat.png";
 
 function App() {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    function onScroll(evt) {
+      setIsScrolling(true);
+      setTimeout(() => { setIsScrolling(false) }, 700);
+    }
+
+    window.addEventListener('scroll', onScroll);
+
+    return function cleanUp() {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   return (
     <div className="App" id="top">
       <BrowserRouter>
@@ -18,13 +35,14 @@ function App() {
         </div>
         <div className="App-container"><About /></div>
         <div className="App-divider">
-          <span className="App-divider-line" id="projects"/>
+          <span className="App-divider-line" id="projects" />
         </div>
         <div className="App-container"><Projects /></div>
         <div className="App-divider">
-          <span className="App-divider-line" id="contact"/>
+          <span className="App-divider-line" id="contact" />
         </div>
         <div className="App-container"><Contact /></div>
+        <img className="App-scrollcat" src={isScrolling ? scrollCat : noscrollCat} alt="scrollcat" />
       </BrowserRouter>
     </div>
   );
